d1479f237a69c9545481176abd98713ae989db29
Al33f <47986891+Al33f@users.noreply.github.com>
2025-06-21 16:44:29 -0400

    Small tweaks. Auth done.

--- app/(auth)/sign-up/sign-up-form.tsx
+++ app/(auth)/sign-up/sign-up-form.tsx
@@ -88,7 +88,7 @@ const SignUpForm = () => {
 
         <div className="text-sm text-center text-muted-foreground">
           Already have an account?{" "}
-          <Link href="/sign-in" target="_self" className="link">
+          <Link href="/sign-in" target="_self" className="link underline">
             Sign In
           </Link>
         </div>
